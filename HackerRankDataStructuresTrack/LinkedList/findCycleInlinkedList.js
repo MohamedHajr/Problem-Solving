508a501f68170710613201decb0b5b76a1625f50
Mohamed Hajr <mohamed.hajr2@gmail.com>
2018-03-24 20:49:00 +0200

    Solving finding a cycle problem in a linkedlist

---
+++ HackerRankDataStructuresTrack/LinkedList/findCycleInlinkedList.js
@@ -0,0 +1,29 @@
+/*
+    Detect a cycle in a Linked List.
+    Note that the head may be 'null' if the list is empty.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function hasCycle(head) {
+    if(head == null) return false
+
+    let slow = head
+    let fast = head
+
+    while (fast != null && fast.next != null){
+        slow = slow.next
+        fast = fast.next.next
+
+        if (slow == fast) return true
+    }
+    return false
+}
+
+
