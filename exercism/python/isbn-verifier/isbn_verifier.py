96dd34d595bb503c5ed1180c5e38c23082fdacdf
Mohamed Hajr <mohamed.hajr2@gmail.com>
2018-06-02 17:55:53 +0200

    Sovling verify ispn problem

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,14 @@
+import re
+
 def verify(isbn):
-    pass
+    strippedIsbn = isbn.replace('-', '')
+    if not re.compile("[0-9]{9}([0-9]|X)$").match(strippedIsbn):
+        return False
+    return isIsbn(strippedIsbn, 0, 10)    
+
+
+def isIsbn(isbn, result, calculationStepValue):
+    if len(isbn) == 0 : return (result % 11) == 0
+    element = int(isbn[0]) if isbn[0] != 'X' else 10
+    newResult = element * calculationStepValue + result
+    return isIsbn(isbn[1:], newResult, calculationStepValue - 1)
