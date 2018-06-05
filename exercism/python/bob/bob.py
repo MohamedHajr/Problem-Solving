5f607081ff86495cd4409ec5afcf401a0ab84011
Mohamed Hajr <mohamed.hajr2@gmail.com>
2018-06-05 16:10:02 +0200

    Solving bob problem

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,2 +1,27 @@
+import re
+
+
+def respond_to_question(s):
+    return 'Sure.' if s.endswith('?') else None
+
+
+def respond_to_noword(s):
+    return "Fine. Be that way!" if s == '' else None
+
+
+def respond_to_yelling(s):
+    if s.isupper():
+        if s[-1] == '?':
+            return 'Calm down, I know what I\'m doing!'
+        else:
+            return 'Whoa, chill out!'
+    else:
+        return None
+
+
 def hey(phrase):
-    pass
+    stripped = phrase.strip()
+    return (respond_to_noword(stripped)
+            or respond_to_yelling(stripped)
+            or respond_to_question(stripped)
+            or 'Whatever.')
