dedc0c74f4cba6b98e5d60321361d37b39814e0c
Mohamed Hajr <mohamed.hajr2@gmail.com>
2018-06-23 23:32:52 +0200

    Solving run-length-encoding on exercism

--- exercism/python/run-length-encoding/run_length_encoding.py
+++ exercism/python/run-length-encoding/run_length_encoding.py
@@ -1,33 +1,14 @@
+import re
+from itertools import groupby
+
 def decode(string):
-    if string == '': return ''
-    elif string.isalpha(): return string
-    else : 
-        if string[0].isdigit() and not string[1].isdigit(): 
-            print(int(string[0]) * string[1])
-            return int(string[0]) * string[1] + decode(string[2:])
-        elif string[0].isdigit() and string[1].isdigit():
-            print(string[3:])
-            return int(string[0]+ string[1]) * string[2] + decode(string[3:])
-        elif not string[0].isdigit():
-            return string[0] + decode(string[1:])
+    groups = re.findall(r'([0-9]*)(\w| )', string)
+    return "".join([char * int(count or 1) for (count, char) in groups])
+
 
 def encode(string):
-    if string == '': return ''
-    return try_encode_it(string, string[0], 0)
+    groups = groupby(string)
+    return "".join([try_encode(list(group)) for key, group in groups])
 
-
-def try_encode_it(string, current_letter, current_counter):
-    
-    if string == ''  and current_counter == 1: return current_letter 
-    elif string == '' and current_counter > 1:
-        return str(current_counter) + current_letter
-
-    elif string[0] == current_letter:
-        return try_encode_it(string[1:], current_letter, current_counter+1 )
-    else :
-        new_string = string[1:]
-        if current_counter == 1:
-            return current_letter + try_encode_it(new_string, string[0], 1)
-        else: 
-            return str(current_counter) + current_letter + try_encode_it(new_string,
-                    string[0], 1)
+def try_encode(chars): 
+    return f"{str(len(chars)) if len(chars) > 1 else ''}{chars[0]}"
