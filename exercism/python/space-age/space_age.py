140db0ae0ee60eec98eb995b486c4b89185b8b10
Mohamed Hajr <mohamed.hajr2@gmail.com>
2018-07-24 13:50:03 +0200

    Solving space age on exercism

--- exercism/python/space-age/space_age.py
+++ exercism/python/space-age/space_age.py
@@ -1,3 +1,24 @@
+plants_years = {
+    'mercury':  0.2408467,
+    'venus':  0.61519726,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132,
+    'earth': 1
+}
+
+
 class SpaceAge(object):
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+        for planet in plants_years:
+            self.add_function(planet)
+
+    def add_function(self, planet):
+        setattr(self, 'on_' + planet,
+                lambda: self.calculate_years(planet))
+
+    def calculate_years(self, planet):
+        return round(self.seconds / 31557600 / plants_years[planet], 2)
