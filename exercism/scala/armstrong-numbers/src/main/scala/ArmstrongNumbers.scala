633942e4888c6821b808da66f120d1d5ea5ab96b
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-07-20 11:39:35 +0200

    more expressive Armsrong-numbers solution scala-track

--- exercism/scala/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
+++ exercism/scala/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -1,9 +1,10 @@
 import math.pow
 
 object ArmstrongNumbers {
-    def digits(num: Int): List[Int] = num.toString.map(_.asDigit).toList
     def isArmstrongNumber(num: Int): Boolean = {
-        val digitsList = digits(num)
-        digitsList.fold(0)((acc, x) => acc + pow(x,digitsList.size).toInt)  == num
+        val digits =  num.toString
+        val digitalize: Char => Int = _.asDigit
+        val power: Int => Double = pow(_, digits.size)
+        digits.map(digitalize andThen power).sum == num
     }
 }
