4a0be008383eed5ba15feaee3a17ce61d4f52911
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-07-30 11:27:59 +0200

    Optmizing nth-prime scala-track

--- exercism/scala/nth-prime/src/main/scala/NthPrime.scala
+++ exercism/scala/nth-prime/src/main/scala/NthPrime.scala
@@ -1,13 +1,11 @@
 object NthPrime {
-  def getNextPrime(prime: Long): Stream[Long] = 
-      Stream.iterate(prime + 2)(_ + 2).filter(isPrime)
     
-  val primes: Stream[Long] = Stream.cons[Long](2, getNextPrime(1))
+  val primes: Stream[Int] = 2 #:: Stream.from(3, 2).filter(isPrime)
 
-  def isPrime(number: Long): Boolean = 
+  def isPrime(number: Int): Boolean = 
       primes.takeWhile(_ <= math.sqrt(number)).forall(number % _ != 0)
    
-  def prime(n: Int): Option[Long] = n match {
+  def prime(n: Int): Option[Int] = n match {
     case 0 => None
     case _ => Some(primes.take(n).last)
   }
