dab621e9e73ae58bf23e4071c7337f06b553db51
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-01 10:47:46 +0200

    WIP PalindromeProducts scala-track

--- exercism/scala/palindrome-products/src/main/scala/PalindromeProducts.scala
+++ exercism/scala/palindrome-products/src/main/scala/PalindromeProducts.scala
@@ -1,1 +1,23 @@
-case class PalindromeProducts(:)
+import scala.annotation.tailrec
+case class PalindromeProducts(start: Int, end: Int) {
+  type Result = (Int, Set[(Int, Int)])
+  def isPali(number: Int): Boolean = {
+    @tailrec def reverse(n: Int, acc: Int = 0): Int = 
+      if(n <= 0) acc
+      else reverse(n / 10, acc * 10 + n % 10)
+    reverse(number) == number
+  }
+
+  lazy val palindromes: List[Result] = {
+    (start to end).foldLeft(List[Result]()) {(acc, n) =>
+      val xs = (1 to n).foldLeft(Set[(Int, Int)]()){ (acc, x) => 
+        if(isPali(n * x)) acc.+((x, n))
+        else acc
+      } 
+      (n, xs) :: acc
+    }
+  }
+
+  def smallest: Option[Result] = Some(palindromes.last)
+  def largest: Option[Result] = Some(palindromes.head)
+}
