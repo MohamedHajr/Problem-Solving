049589e494dc2f29831f7548b5ad1061f526aee5
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-04 15:45:32 +0200

    Optimizing grade-school scala-track

--- exercism/scala/grade-school/src/main/scala/School.scala
+++ exercism/scala/grade-school/src/main/scala/School.scala
@@ -1,14 +1,12 @@
 class School {
   type DB = Map[Int, Seq[String]]
 
-  private var db: DB =  Map()
+  var db: DB =  Map.empty
 
-  def add(name: String, g: Int) =  {
-    db = db + (g -> (grade(g).:+(name)))
-  }
+  def add(name: String, g: Int) =  db += g -> (grade(g) :+ name)
 
   def grade(g: Int): Seq[String] = db.getOrElse(g, Seq())
 
-  def sorted: DB =  Map(db.toSeq.sortBy(_._1): _*).map{ case (key, value) => (key, value.sorted) } 
+  def sorted =  collection.SortedMap(db.toSeq:_*).mapValues(_.sorted) 
 
 }
