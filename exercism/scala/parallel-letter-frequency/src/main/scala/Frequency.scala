6cb9753c69ba6571b206c0012b0d878b16dc7175
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-06 13:17:03 +0200

    WIP parallel letter frequency

--- exercism/scala/parallel-letter-frequency/src/main/scala/Frequency.scala
+++ exercism/scala/parallel-letter-frequency/src/main/scala/Frequency.scala
@@ -1,8 +1,16 @@
 object Frequency {
+  implicit class improvedMap[A](val zis: Map[A, Int])(implicit num: Numeric[Int]) {
+    def combine(that: Map[A, Int]): Map[A, Int] =  {
+      val merged = zis.toSeq ++ that.toSeq
+      val grouped = merged.groupBy(_._1)
+      grouped.mapValues(_.map(_._2).sum)
+    }
+  }
   def calcFrequency(text: String): Map[Char, Int] = text.groupBy(identity).mapValues(_.size)
+
   def frequency(numWorkers: Int, texts: Seq[String]): Map[Char, Int] =  texts
-    .map(_.toLowerCase.trim)
-    .map(_.filter(_.isLetter))
+    .map(_.filter(_.isLetter).toLowerCase.trim)
     .map(calcFrequency)
-    .fold(Map())(_ ++ _)
+    .fold(Map())(_ combine _)
 }
+
