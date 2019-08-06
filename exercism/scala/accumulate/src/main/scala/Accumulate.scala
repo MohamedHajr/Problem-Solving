48b80b4ad2a231cf176b3633ebcbeba73b71c717
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-06 23:58:52 +0200

    Refactoring accumlate scala-track

--- exercism/scala/accumulate/src/main/scala/Accumulate.scala
+++ exercism/scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,10 @@
+import scala.annotation.tailrec
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = list.map(f) 
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+    @tailrec def transfer(inialList: List[A], resultList: List[B]): List[B] = inialList.headOption match {
+      case None => resultList
+      case Some(a) => transfer(inialList.tail, f(a) :: resultList) 
+    } 
+    transfer(list, List.empty).reverse
+  }
 }
