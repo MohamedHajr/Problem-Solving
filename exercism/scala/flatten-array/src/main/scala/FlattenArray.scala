b4e6a87dd93419ed4c2f08e11bf3ef8b564f8d66
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-07 13:27:56 +0200

    Solving flattenArray scala-track

--- exercism/scala/flatten-array/src/main/scala/FlattenArray.scala
+++ exercism/scala/flatten-array/src/main/scala/FlattenArray.scala
@@ -1,9 +1,7 @@
 object FlattenArray {
-  def flatten(current: List[Any], acc: List[Any] = List.empty): List[Any] = current match {
-    case Nil => acc
-    case null :: xs => flatten(xs, acc)
-    case (y :: ys) :: xs => flatten(y :: ys) ::: flatten(xs, acc)
-    case x :: xs => flatten(xs, x :: acc)
+  def flatten(current: List[Any]): List[Int] = current flatMap {
+    case y :: ys => flatten(y :: ys)
+    case x: Int => List(x)
+    case _ => List()
   }
-
 }
