604941b79ba4a01bd8bdff6ed0ebcda36addf2a5
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-07 23:30:14 +0200

    Optimizing series scala-track

--- exercism/scala/series/src/main/scala/Series.scala
+++ exercism/scala/series/src/main/scala/Series.scala
@@ -1,6 +1,8 @@
 import scala.annotation.tailrec
 object Series {
-  def slices(slices: Int, series: String): List[List[Int]] = {
+  def slices(slices: Int, series: String): List[Seq[Int]] = series.map(_.asDigit).sliding(slices).toList
+
+  def slicesNaive(slices: Int, series: String): List[List[Int]] = {
     @tailrec def go(acc: List[List[Int]], series: String): List[List[Int]] = series.size match {
       case size if size < slices => acc
       case s => go(series.take(slices).toList.map(_.asDigit) :: acc, series.drop(1))
