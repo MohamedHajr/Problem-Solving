560cf6124f488ac4ea149321160a33582c0a8ab9
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-07 12:18:44 +0200

    Optimizing accumlate scala-track

--- exercism/scala/accumulate/src/main/scala/Accumulate.scala
+++ exercism/scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,9 +1,9 @@
 import scala.annotation.tailrec
 class Accumulate {
   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
-    @tailrec def transfer(inialList: List[A], resultList: List[B]): List[B] = inialList.headOption match {
-      case None => resultList
-      case Some(a) => transfer(inialList.tail, f(a) :: resultList) 
+    @tailrec def transfer(inialList: List[A], resultList: List[B]): List[B] = inialList match {
+      case Nil => resultList
+      case x::xs => transfer(xs, f(x) :: resultList) 
     } 
     transfer(list, List.empty).reverse
   }
