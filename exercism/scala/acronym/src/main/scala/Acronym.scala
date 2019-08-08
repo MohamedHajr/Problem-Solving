b807904bfeea66d571d289a356265fc8f6571dce
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-08 16:59:08 +0200

    Solving acronym scala-track

--- exercism/scala/acronym/src/main/scala/Acronym.scala
+++ exercism/scala/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,7 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def abbreviate(phrase: String): String = phrase
+    .split("[ -]")
+    .filterNot(_.isEmpty)
+    .map { case word => word.head.toUpper }
+    .mkString
 }
