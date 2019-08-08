9ece1ef10df50095533cf75cc8dca926b02ac6a2
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-08 19:24:39 +0200

    Solving run-length-encoding scala-track

--- exercism/scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ exercism/scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -2,16 +2,20 @@ object RunLengthEncoding {
   def encode(text: String, acc: String = ""): String =  text match {
     case "" => acc
     case _ =>  {
-     val matches = text.takeWhile(text.head == _)
-     if(matches.size > 1)
-      encode(text.drop(matches.size), acc ++ s"${matches.size}${matches.head}")
-     else
-      encode(text.drop(1), acc ++ matches.head.toString)   
+     val (matches, rest) = text.span(_ == text.head)
+     val encodedChunk = if (matches.length == 1) matches else matches.length.toString + matches.head
+     encode(rest, acc ++ encodedChunk)
     }
   }
    
   
-  def decode(cypher: String): String = {
-    
+  def decode(cypher: String, decoded: String = ""): String = cypher match {
+    case "" => decoded
+    case _ => {
+      val (number, rest) = cypher.span(_.isDigit)
+      val decodedChunk = if(number == "") rest.head.toString else rest.head.toString * number.toInt 
+      decode(rest.tail, decoded ++ decodedChunk)
+    }
   }
+     
 }
