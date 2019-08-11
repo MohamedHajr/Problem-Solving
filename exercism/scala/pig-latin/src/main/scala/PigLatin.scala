fb33fa5ed32bef04a388766e952ba1d965e78939
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-11 23:12:25 +0200

    Solving pig-latin scala-track

--- exercism/scala/pig-latin/src/main/scala/PigLatin.scala
+++ exercism/scala/pig-latin/src/main/scala/PigLatin.scala
@@ -3,6 +3,7 @@ object PigLatin {
 
   private val specialCases = Set("ch", "qu", "th", "thr", "sch")
 
+  //pattern matching version
   def translateWord(word: String): String = word.toList match {
     case x :: y :: _  if (vowelsAndCases contains x) || (vowelsAndCases contains s"$x$y") => word ++ "ay"
     case x :: y :: z :: _ if specialCases contains(s"$x$y$z") => word.drop(3) ++ s"$x$y$z" ++ "ay"
@@ -11,5 +12,17 @@ object PigLatin {
     case _ => word.drop(1) ++ word.take(1) ++ "ay"
   }
 
-  def translate(word: String): String = word.split(" ").map(translateWord).mkString(" ")
+  //regex version
+  def translateWord2(word: String): String = {
+    val specialVowelsRegex = "^(xr|yt)(.*)$".r  
+    val constant = """^(qu|y|[^aeiouy]qu|[^aeiouy]+)(.*)""".r
+
+    word match {
+      case specialVowelsRegex(first, last) => first ++ last ++ "ay"
+      case constant(first, last) => last ++ first ++ "ay"
+      case _ => word + "ay"
+    }
+  }
+
+  def translate(word: String): String = word.split(" ").map(translateWord2).mkString(" ")
 }
