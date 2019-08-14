1c1d44db2a31e9d071d38cc3c42b4366304dbee8
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-14 23:37:16 +0200

    Optimizing etl scala-track

--- exercism/scala/etl/src/main/scala/Etl.scala
+++ exercism/scala/etl/src/main/scala/Etl.scala
@@ -1,5 +1,4 @@
 object Etl {
   def transform(oldData: Map[Int, Seq[String]]): Map[String, Int] = oldData
-    .map { case (key, seq) => seq.map(_.toLowerCase -> key).toMap }
-    .fold(Map.empty)(_ ++ _)
+    .flatMap { case (key, seq) => seq.map(_.toLowerCase -> key).toMap }
 }
