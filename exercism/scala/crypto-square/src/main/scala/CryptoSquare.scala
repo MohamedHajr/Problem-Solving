70d857bb69932844c647276cc7c80b4a14910d48
Mohamed Hajr <mohamed.hajr2@gmail.com>
2019-08-17 17:16:58 +0200

    Solved crypt-square scala-track

--- exercism/scala/crypto-square/src/main/scala/CryptoSquare.scala
+++ exercism/scala/crypto-square/src/main/scala/CryptoSquare.scala
@@ -5,8 +5,8 @@ object CryptoSquare {
 
   @tailrec def findMultiples(n: Int, acc: Int): (Int, Int) = {
     val squareOfN = math.sqrt(n).ceil.toInt
-    val multiples = (2 to squareOfN) find (x => n / x - x == 1)
-    multiples match {
+    val multiple = (2 to squareOfN) find { x => n % x == 0 &&  n / x - x <= 1 }
+    multiple match {
       case None => findMultiples(n + 1, acc + 1)
       case Some(x) => (n / x, acc)
     }
@@ -15,12 +15,10 @@ object CryptoSquare {
   @tailrec def transpose(words: List[String], result: List[String]): List[String] = words.head.isEmpty match {
     case true => result
     case _ => { 
-      println(words)
       transpose(words map (_.tail), (words map (_.head) mkString) :: result)
     }
   }
     
-  
   def ciphertext(text: String): String = {
     val normalized = normalize(text)
     if(normalized.size <= 1) 
@@ -28,7 +26,7 @@ object CryptoSquare {
     else {
       val (row, margin) = findMultiples(normalized.size, 0)
       val textAfterPadding = normalized ++ (" " * margin)
-      val transposed = transpose(textAfterPadding.sliding(row).toList, List.empty)
+      val transposed = transpose(textAfterPadding.grouped(row).toList, List.empty)
       transposed.reverse.mkString(" ") 
     }
   } 
